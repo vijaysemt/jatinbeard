3be55a9fb4ccc4a3b7f34950ee1f9039420a6490
vijay <vijaysainisemt@gmail.com>
2024-12-01 01:47:10 +0530

    date issue

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Order;
 use App\Models\Product;
+use Carbon\Carbon;
 
 use Illuminate\Support\Facades\Log; // Add this line to import the Log facade
 class HomeController extends Controller
@@ -43,14 +44,17 @@ class HomeController extends Controller
             $startDate = now()->startOfMonth();
             // Get today's date
             $endDate = now();
-           
+            $startDate = Carbon::parse($startDate)->startOfDay();  // Ensure start date is at the beginning of the day
+            $endDate = Carbon::parse($endDate)->endOfDay();  // Ensure end date is at the end of the day (23:59:59)
+            
         } 
         // Log::info('start date');
         // Log::info($startDate);
         // Log::info('endDate');
         // Log::info($endDate);
         // Fetch orders between the given date range
-        $orders = Order::whereBetween('created_at', [$startDate, $endDate])->get();
+        $orders = Order::where('created_at', '>=', $startDate) // Start date filter
+        ->where('created_at', '<=', $endDate)->get();
 
         // Calculate total orders and amounts
         $totalOrders = $orders->count();
